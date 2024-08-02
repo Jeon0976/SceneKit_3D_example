a99e1756dd8d27887e911992d5272eb1cef9c988
seonghun jeon <seonghun.jeon@illuminarean.com>
2024-08-02 15:44:58 +0900

    Initial Commit

---
+++ SolarSystemUsingSceneKit/SolarSystemUsingSceneKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemUsingSceneKitApp.swift
+//  SolarSystemUsingSceneKit
+//
+//  Created by 전성훈 on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemUsingSceneKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
